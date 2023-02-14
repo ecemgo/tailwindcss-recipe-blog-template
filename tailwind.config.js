adfbcb4e6cc24f281e826be3ec5300610a6331c3
Ecem Gokdogan <ecemgo@hotmail.com>
2023-02-14 16:17:58 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
+  },
+  plugins: [],
+};
